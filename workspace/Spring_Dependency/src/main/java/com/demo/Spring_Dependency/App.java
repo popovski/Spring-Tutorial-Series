f498c68d1eb4922822f43a537fd331a20f568a7c
Nikola Popovski <popovski.nikola@gmail.com>
2017-11-01 23:41:28 +0100

    Spring dependency injection project

---
+++ workspace/Spring_Dependency/src/main/java/com/demo/Spring_Dependency/App.java
@@ -0,0 +1,13 @@
+package com.demo.Spring_Dependency;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
